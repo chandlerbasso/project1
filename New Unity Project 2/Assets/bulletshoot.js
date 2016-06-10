9b51257561826d564e697a7b1df42c747e074e98
Chandler Basso <cebasso@asu.edu>
2016-06-09 20:08:06 -0400

    started my space shooter, finished basic functionality

---
+++ New Unity Project 2/Assets/bulletshoot.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var speed = 20;
+function Start () {
+GetComponent.<Rigidbody2D>().velocity.y = speed;
+}
+
+function OnBecomeInvisible() {
+Destroy(gameObject);
+}
---
+++ New Unity Project 2/Assets/movment.js
@@ -0,0 +1,48 @@
+﻿//i refrenced http://docs.unity3d.com/ScriptReference/ for unityscript syntax
+
+
+
+#pragma strict
+
+var move = 5 ;
+
+var rotationspeed = 45;
+
+
+function start () {
+
+
+}
+
+
+function Update () {
+//ship movment forward
+
+
+if (Input.GetKey(KeyCode.W))
+{
+GetComponent.<Rigidbody2D>().velocity.y = move;
+
+}
+//ship moves backwards
+else if (Input.GetKey(KeyCode.S))
+{
+GetComponent.<Rigidbody2D>().velocity.y = -move;
+
+}
+else{
+GetComponent.<Rigidbody2D>().velocity.y = 0;
+}
+//ship turns right
+
+ if (Input.GetKey(KeyCode.A)){
+     GetComponent.<Rigidbody2D>().transform.Rotate(Vector3.forward * rotationspeed * Time.deltaTime);
+
+}
+//ship turns left
+else if (Input.GetKey(KeyCode.D)){
+     GetComponent.<Rigidbody2D>().transform.Rotate(Vector3.back * rotationspeed * Time.deltaTime);
+
+}
+
+}
